bb7560953aedcd7b57d8db99b951238980a00f8d
abhishek986873 <110660543+abhishek986873@users.noreply.github.com>
2022-08-06 21:14:01 +0530

    Add files via upload

---
+++ area of trapezium.py
@@ -0,0 +1,8 @@
+a=input("height of trapezium = ")
+b=input("length of 1st parallel side = ")
+c=input("length of 2nd parellel side = ")
+a=float(a)
+b=float(b)
+c=float(c)
+d=((b+c)/2)*a
+print("area of trapezium=",d)
